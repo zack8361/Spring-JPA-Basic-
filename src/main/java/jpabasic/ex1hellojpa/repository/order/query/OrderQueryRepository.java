f82f8b4f84e3780ac97eaf52f22820299b42f849
zack8361 <71763096+zack8361@users.noreply.github.com>
2024-02-06 18:39:00 +0900

    null 값 수정
    null 값 수정

--- src/main/java/jpabasic/ex1hellojpa/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabasic/ex1hellojpa/repository/order/query/OrderQueryRepository.java
@@ -11,9 +11,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderQueryRepository {
     private final EntityManager em;
-    public List<OrderQueryDto> findOrderQueryDtos(){
+
+    public List<OrderQueryDto> findOrderQueryDtos() {
         List<OrderQueryDto> result = findOrders();
-        result.forEach(o->{
+        result.forEach(o -> {
             List<OrderItemQueryDto> orderItems = findOrderItems(o.getOrderId());
             o.setOrderItems(orderItems);
         });
@@ -22,17 +23,19 @@ public class OrderQueryRepository {
 
     private List<OrderItemQueryDto> findOrderItems(Long orderId) {
         return em.createQuery("SELECT " +
-                "new jpabasic.ex1hellojpa.repository.order.query.OrderItemQueryDto(oi.order.id, i.name,oi.orderPrice,oi.count) FROM OrderItem oi JOIN oi.item i WHERE oi.id = :orderId", OrderItemQueryDto.class)
-                .setParameter("orderId",orderId)
+                        "new jpabasic.ex1hellojpa.repository.order.query.OrderItemQueryDto(oi.order.id, i.name,oi.orderPrice,oi.count) " +
+                        "FROM OrderItem oi " +
+                        "JOIN oi.item i " +
+                        "WHERE oi.id = :orderId", OrderItemQueryDto.class)
+                .setParameter("orderId", orderId)
                 .getResultList();
     }
 
     public List<OrderQueryDto> findOrders() {
-        em.createQuery("SELECT new jpabasic.ex1hellojpa.repository.order.query.OrderQueryDto(o.id,m.userName,o.orderDate,o.orderStatus,d.address) " +
+       return em.createQuery( "SELECT new jpabasic.ex1hellojpa.repository.order.query.OrderQueryDto(o.id,m.userName,o.orderDate,o.orderStatus,d.address) " +
                         "from Order o " +
                         "JOIN o.member m " +
                         "JOIN o.delivery d", OrderQueryDto.class)
                 .getResultList();
-        return null;
     }
 }
